5ca2302aab8aba4594ecae0437b9e0322d07b04b
Samer Jarmakani <samer.jarmakani@gmail.com>
2020-01-15 23:53:20 +1030

    Added jQuery, DateTime Picker, Carbon

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Datetime Test</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
         <style>
             body {
                 margin-top: 1rem;
@@ -18,11 +19,26 @@
 					<div class="card">
 						<div class="card-body">
 							<h3 class="card-title">Datetime Test by Samer Jarmakani</h3>
-							
+							<form>
+								<div class="form-group">
+									<label for="datetime1">First Date</label>
+									<input type="text" class="form-control datetime" id="datetime1">
+								</div>
+								<div class="form-group">
+									<label for="datetime2">Second Date</label>
+									<input type="text" class="form-control datetime" id="datetime2">
+								</div>
+								<button type="submit" class="btn btn-primary">Submit</button>
+							</form>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
     </body>
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/bootstrap-datetimepicker.min.js"></script>
+    <script>
+	    $(".datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+	</script>
 </html>
